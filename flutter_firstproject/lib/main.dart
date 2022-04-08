f469e73f20b5c90a5b7247c28123e21ff80e6d85
Garrett <williamhootengarrettgmail.com>
2022-04-08 17:49:47 -0400

    Added profile repository and some plugins

--- flutter_firstproject/lib/main.dart
+++ flutter_firstproject/lib/main.dart
@@ -1,116 +1,79 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_storage_s3/amplify_storage_s3.dart';
 import 'package:flutter/material.dart';
-import 'style.dart';
+import 'package:amplify_flutter/amplify.dart';
+
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:provider/provider.dart';
+import 'utils/shared_prefs.dart';
+
+import 'models/ModelProvider.dart';
+import 'amplifyconfiguration.dart';
+import 'profile/profile_repositiory.dart';
+
+import 'screens/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<void> _initializeApp() async {
+    await _configureAmplify();
+  }
+
+  final AmplifyDataStore _amplifyDataStore = AmplifyDataStore(
+    modelProvider: ModelProvider.instance,
+  );
+  Future<void> _configureAmplify() async {
+    try {
+      await Amplify.addPlugins([
+        _amplifyDataStore,
+        AmplifyAuthCognito(),
+        AmplifyAPI(),
+        AmplifyStorageS3()
+      ]);
+
+      // Once Plugins are added, configure Amplify
+      await Amplify.configure(amplifyconfig);
+    } catch (e) {
+      print('an error occured during amplify configuration: $e');
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _initializeApp();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: _theme(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-
-  ThemeData _theme() {
-    return ThemeData(
-        appBarTheme: AppBarTheme(
-          titleTextStyle: AppBarTextStyle,
-        ),
-        textTheme: TextTheme(
-          titleMedium: TitleTextStyle,
-          bodyMedium: Body1TextStyle,
-        ));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: 'Friends',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primaryColor: const Color(0xFF1c1c1c),
+            accentColor: const Color(0XFFf94c84),
+            fontFamily: 'Montserrat'),
+        home: MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (_) => ProfileRepository.instance(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ChangeNotifierProvider(
+              create: (_) => SharedPrefsUtils.instance(),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          child: Home(),
+        ));
   }
 }
