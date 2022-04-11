d79e1fa41d19f71456a5df245864e06212709a04
Garrett <williamhootengarrett@gmail.com>
2022-04-11 14:39:30 -0400

    Added files for testing (will remove in update)

---
+++ flutter_firstproject/lib/screens/register.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firstproject/profile/profile_repository.dart';
+import 'package:flutter_firstproject/screens/otp_screen.dart';
+import 'package:provider/provider.dart';
+import 'nav_drawer.dart';
+
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'profile.dart';
+import 'register_screen.dart';
+import '../models/ModelProvider.dart';
+import '../profile/profile_repository.dart';
+import '../style.dart';
+import '../utils/shared_prefs.dart';
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_storage_s3/amplify_storage_s3.dart';
+import 'package:flutter/material.dart';
+// Amplify Flutter Packages
+import 'package:amplify_flutter/amplify.dart';
+import 'package:provider/provider.dart';
+
+import '../amplifyconfiguration.dart';
+
+import '../login/login_repository.dart';
+
+class RegisterScreen extends StatelessWidget {
+  RegisterScreen({Key? key}) : super(key: key);
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final loginRep = LoginRepository.instance();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create an Account'),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
+      ),
+      drawer: NavDrawer(),
+
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+              child: Text(
+                'Create a username and password',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: TextField(
+                controller: loginRep.usernameController,
+                //controller: usernameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Username',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+              child: TextField(
+                controller: loginRep.passwordController,
+                //controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Password',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 5),
+              child: FloatingActionButton.extended(
+                onPressed: () {
+                  if (true /* replace this with validation criteria */) {
+                    loginRep.register(context).then((bool? value) {
+                      if (loginRep.isSignUpComplete) {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return OtpScreen(
+                              username: loginRep.usernameController.text.trim(),
+                              password: loginRep.passwordController.text.trim(),
+                              email: loginRep.emailController.text.trim());
+                        }));
+                      } else {}
+                    });
+                  }
+
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        // Retrieve the text the that user has entered by using the
+                        // TextEditingController.
+                        content: Text('Username: ' +
+                            usernameController.text +
+                            '\n' +
+                            'Password: ' +
+                            passwordController.text),
+                      );
+                    },
+                  );
+                },
+                label: const Text('Submit'),
+              ),
+            )
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ flutter_firstproject/lib/utils/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// NOTE: REMOVE THIS! This file is just for
+// Testing premade code and is not part of self-starter
+
+class ThemeColor {
+  static const Color primary = Color(0xFFbb2ace);
+  static const Color bgColor = Color(0xFF1c1c1c);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color textfieldColor = Color(0xFF2b2b2b);
+  static const Color greyColor = Color(0xFF161616);
+  static const Color chatBoxOther = Color(0xFF3d3d3f);
+  static const Color secondary = Color(0xFFf94c84);
+  static const Color color1 = Color(0xFFfa709a);
+  static const Color color2 = Color(0xFFfee240);
+  static const Color cardBackground = Color(0xFF434343);
+
+  static Shader linearGradient = const LinearGradient(
+    colors: <Color>[secondary, primary],
+  ).createShader(const Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
+  static Shader linearGradient2 = const LinearGradient(
+    colors: <Color>[color1, color2],
+  ).createShader(const Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
+}
